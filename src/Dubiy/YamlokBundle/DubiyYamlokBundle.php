41e149020c2a081c3f91ef6c1268e2cff8397025
Igor Dubiy <igor-dubiy@ekreative.com>
2015-12-10 15:57:33 +0200

    file structure

---
+++ src/Dubiy/YamlokBundle/DubiyYamlokBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dubiy\YamlokBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DubiyYamlokBundle extends Bundle
+{
+}
--- src/Dubiy/YamlokBundle/Service/Yamlok.php
+++ src/Dubiy/YamlokBundle/Service/Yamlok.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dubiy\Yamlok;
+namespace Dubiy\YamlokBundle\Service;
 
 
 class Yamlok
